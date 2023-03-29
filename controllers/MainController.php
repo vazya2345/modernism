3ee9d8b12cff4a9b53b4418d8c88fb4fd020cb70
vazya2345 <avaz.tuhtasinov@ya.ru>
2023-03-29 10:34:31 +0500

    lang filter added

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -2,7 +2,8 @@
 namespace app\controllers;
  
 use yii\rest\ActiveController;
- 
+use app\models\Main;
+use app\models\Architector;
 class MainController extends ActiveController
 {
     public $modelClass = 'app\models\Main';
@@ -10,11 +11,51 @@ class MainController extends ActiveController
       return [
         [
           'class' => \yii\filters\ContentNegotiator::className(),
-          'only' => ['index', 'view'],
+          'only' => ['index', 'view', 'getobject'],
           'formats' => [
             'application/json' => \yii\web\Response::FORMAT_JSON,
           ],
         ],
       ];
     }
+
+
+
+
+    public function actionGetobject($id = 3, $lang = 'uz') {
+        $model = Main::findOne($id);
+        $res = [];
+        if($lang == 'ru'){
+          $res['title'] = $model->title_ru;
+          $res['desc'] = $model->desc_ru;
+          $res['address'] = $model->address_ru;
+        }
+        elseif($lang == 'en'){
+          $res['title'] = $model->title_en;
+          $res['desc'] = $model->desc_en;
+          $res['address'] = $model->address_en;
+        }
+        else{
+          $res['title'] = $model->title;
+          $res['desc'] = $model->desc;
+          $res['address'] = $model->address;
+        }
+        $res['photo_url'] = $model->photo_url;
+        $res['arcitector_id'] = $model->arcitector_id;
+        $res['arcitector_name'] = Architector::getName($model->arcitector_id);
+
+        if($model->geolocation != NULL){
+          $coordinates = explode(", ", $model->geolocation);
+        }
+        $res['geolocation'] = ['latitude' => $coordinates[0], 'longitude' => $coordinates[1]];
+
+        $res['video_presentation_url'] = $model->video_presentation_url;
+        $res['threed_model_url'] = $model->threed_model_url;
+        $res['build_period'] = $model->build_period;
+
+        return $res;
+
+
+    }
+
 }
